d59592216e31de8a92c2a82272f415bbc1ba0546
Gerges hanna FCI-H <gogohanna50@gmail.com>
2020-03-31 03:53:13 +0200

    10 Book

--- NormalClasses/Book.php
+++ NormalClasses/Book.php
@@ -1,6 +1,6 @@
 <?php
 
-include './Category.php';
+include '../NormalClasses/Category.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
