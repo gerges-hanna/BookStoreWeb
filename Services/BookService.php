ded4cc0d4a6d95d5ac4b331907909c7b52df4590
Gerges hanna FCI-H <gogohanna50@gmail.com>
2020-03-31 02:27:30 +0200

    8 BookService

---
+++ Services/BookService.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BookService
+ *
+ * @author Gerges hanna FCI-H
+ */
+class BookService {
+    //put your code here
+}
